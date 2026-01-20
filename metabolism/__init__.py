889b3076baa033e41874f227f1a30272093008a5
sharmainechia7772-cyber <sharmainechia7772@gmail.com>
2026-01-20 22:35:53 +1300

    Update __init__.py

--- metabolism/__init__.py
+++ metabolism/__init__.py
@@ -1,2 +1,17 @@
 # metabolism/__init__.py
 # Status: The Circulation System of Hope
+
+
+
+
+# metabolism/__init__.py
+from .cognitive_dividend import CognitiveBandwidthEngine
+from .entropy_sink import NegativeEntropyPortal
+from .vitality_monitor import VitalityDetector
+# NEW: Reality Anchors
+from .reality_anchor import MetabolicAnchor, RiverbedDynamics
+
+__all__ = [
+    'CognitiveBandwidthEngine', 'NegativeEntropyPortal', 
+    'VitalityDetector', 'MetabolicAnchor', 'RiverbedDynamics'
+]
